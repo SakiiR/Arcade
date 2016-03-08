e310e574a9b59891f1335ffabc052935fa51d259
Erwan Dupard <erwan.dupard@epitech.eu>
2016-03-08 15:16:15 +0100

    Adding ressources.hh

--- main.cpp
+++ main.cpp
@@ -5,12 +5,34 @@
 // Login   <dupard_e@epitech.net>
 // 
 // Started on  Tue Mar  8 14:40:10 2016 Erwan Dupard
-// Last update Tue Mar  8 14:40:33 2016 Erwan Dupard
+// Last update Tue Mar  8 15:15:52 2016 Erwan Dupard
 //
+
+
+#include "ressources.hh"
+
+void			usage(const char *execName)
+{
+  std::cout << "//" << std::endl;
+  std::cout << "//     _____                            .___" << std::endl;      
+  std::cout << "//    /  _  \\_______   ____ _____     __| _/____" << std::endl;
+  std::cout << "//   /  /_\\  \\_  __ \\_/ ___\\\\__  \\   / __ |/ __ \\ " << std::endl;
+  std::cout << "//  /    |    \\  | \\/\\  \\___ / __ \\_/ /_/ \\  ___/ " << std::endl;
+  std::cout << "//  \\____|__  /__|    \\___  >____  /\\____ |\\___ >" << std::endl;
+  std::cout << "//   \\/            \\/     \\/      \\/    \\/ " << std::endl;
+  std::cout << "//" << std::endl;
+  std::cout << "// [*] Usage : " << execName << " <libraryName>" << std::endl; 
+  std::cout << "// [*] You can find libraries in ./lib/ directory." << std::endl;
+  std::cout << "// [*] Arcade by dupard_e && barthe_g" << std::endl;
+  std::cout << "//" << std::endl;
+}
 
 int			main(int argc, char **argv)
 {
-  (void)argc;
-  (void)argv;
-  return (0);
+  if (argc < 2)
+    {
+      usage(argv[0]);
+      return (RETURN_SUCCESS);
+    }
+  return (RETURN_SUCCESS);
 }
---
+++ ressources.hh
@@ -0,0 +1,19 @@
+//
+// ressources.hh for arcade in /home/dupard_e/rendus/cpp_arcade
+// 
+// Made by Erwan Dupard
+// Login   <dupard_e@epitech.net>
+// 
+// Started on  Tue Mar  8 15:08:07 2016 Erwan Dupard
+// Last update Tue Mar  8 15:10:17 2016 Erwan Dupard
+//
+
+#ifndef RESSOURCES_HH_
+# define RESSOURCES_HH_
+
+# include <iostream>
+
+# define RETURN_SUCCESS		(0)
+# define RETURN_FAILURE		(1)
+
+#endif /* ! RESSOURCES_HH_ */
